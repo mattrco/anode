801d94a1d9d9a9e48dd8c6b5af7d7d7d8318bc06
Matt Cottingham <matt.cottingham@gmail.com>
2014-07-03 22:39:37 +0100

    Add graphite TCP output

---
+++ output/graphite.go
@@ -0,0 +1,38 @@
+package output
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/golang/glog"
+
+	"github.com/myrid/anode.exp/data"
+)
+
+type Graphite struct {
+	name    string
+	conn    net.Conn
+	outchan chan data.Datapoint
+}
+
+func (out *Graphite) Init(graphiteHost string, name string, outchan chan data.Datapoint) error {
+	conn, err := net.Dial("tcp", graphiteHost)
+	if err != nil {
+		return err
+	}
+	out.conn = conn
+	out.outchan = outchan
+	out.name = name
+	return nil
+}
+
+func (out *Graphite) Run() {
+	for o := range out.outchan {
+		// Graphite TCP format: <name> <value> <timestamp>\n
+		outs := fmt.Sprintf("%s %f %d\n", out.name, o.Value, o.Timestamp)
+		_, err := fmt.Fprintf(out.conn, outs)
+		if err != nil {
+			glog.Error(err)
+		}
+	}
+}
