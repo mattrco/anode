1d7ec6313cc406a18380b7623102056c83f5ad40
Matt Cottingham <matt.cottingham@gmail.com>
2014-07-12 11:45:42 +0100

    Add tests for debug analyzer

---
+++ analyze/debug_test.go
@@ -0,0 +1,35 @@
+package analyze
+
+import (
+	"testing"
+
+	"github.com/myrid/anode.exp/data"
+)
+
+func TestDebugInit(t *testing.T) {
+	d := Debug{}
+	var input chan data.Datapoint
+	var outputs []chan data.Datapoint
+	if err := d.Init(input, outputs); err != nil {
+		t.Fatalf("Error initializing debug: %s", err)
+	}
+}
+
+func TestDebugRun(t *testing.T) {
+	input := make(chan data.Datapoint)
+	output := make(chan data.Datapoint)
+	outputs := []chan data.Datapoint{output}
+
+	d := Debug{}
+	if err := d.Init(input, outputs); err != nil {
+		t.Fatalf("Error initializing debug: %s", err)
+	}
+	go d.Run()
+
+	input <- data.Datapoint{Value: 1}
+	result := <-output
+
+	if result.Value != 1 {
+		t.Fatalf("Expected 1, got %d", result.Value)
+	}
+}
