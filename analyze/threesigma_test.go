f8a35800ec7c278bff1905b2a48b8db364ccac58
Matt Cottingham <matt.cottingham@gmail.com>
2014-07-13 15:33:00 +0100

    Add basic three sigma tests

---
+++ analyze/threesigma_test.go
@@ -0,0 +1,40 @@
+package analyze
+
+import (
+	"testing"
+
+	"github.com/myrid/anode.exp/data"
+)
+
+func TestThreeSigmaInit(t *testing.T) {
+	ts := ThreeSigma{}
+
+	var input chan data.Datapoint
+	var outputs [4]chan data.Datapoint
+	if err := ts.Init(input, outputs); err != nil {
+		t.Fatalf("Error initializing three sigma: %s", err)
+	}
+}
+
+func TestThreeSigmaRun(t *testing.T) {
+
+	input := make(chan data.Datapoint)
+	o1 := make(chan data.Datapoint, 1)
+	o2 := make(chan data.Datapoint, 1)
+	o3 := make(chan data.Datapoint, 1)
+	o4 := make(chan data.Datapoint, 1)
+	outputs := [4]chan data.Datapoint{o1, o2, o3, o4}
+
+	ts := ThreeSigma{}
+	if err := ts.Init(input, outputs); err != nil {
+		t.Fatalf("Error initializing three sigma: %s", err)
+	}
+	go ts.Run()
+
+	input <- data.Datapoint{Value: 1}
+	result := <-o2
+
+	if result.Value != 1 {
+		t.Fatalf("Expected 1, got %d", result.Value)
+	}
+}
