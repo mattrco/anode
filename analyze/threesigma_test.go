7e458dfb2a588d2a9b11ecc94b83633a9a578e8d
Matt Cottingham <matt.cottingham@gmail.com>
2014-07-16 17:12:59 +0100

    Update three sigma test to use new API

--- analyze/threesigma_test.go
+++ analyze/threesigma_test.go
@@ -10,29 +10,24 @@ func TestThreeSigmaInit(t *testing.T) {
 	ts := ThreeSigma{}
 
 	var input chan data.Datapoint
-	var outputs [4]chan data.Datapoint
-	if err := ts.Init(input, outputs); err != nil {
+	var output chan data.Datapoint
+	if err := ts.Init(input, output); err != nil {
 		t.Fatalf("Error initializing three sigma: %s", err)
 	}
 }
 
 func TestThreeSigmaRun(t *testing.T) {
-
-	input := make(chan data.Datapoint)
-	o1 := make(chan data.Datapoint, 1)
-	o2 := make(chan data.Datapoint, 1)
-	o3 := make(chan data.Datapoint, 1)
-	o4 := make(chan data.Datapoint, 1)
-	outputs := [4]chan data.Datapoint{o1, o2, o3, o4}
+	input := make(chan data.Datapoint, 1)
+	output := make(chan data.Datapoint, 1)
 
 	ts := ThreeSigma{}
-	if err := ts.Init(input, outputs); err != nil {
+	if err := ts.Init(input, output); err != nil {
 		t.Fatalf("Error initializing three sigma: %s", err)
 	}
 	go ts.Run()
 
 	input <- data.Datapoint{Value: 1}
-	result := <-o2
+	result := <-output
 
 	if result.Value != 1 {
 		t.Fatalf("Expected 1, got %d", result.Value)
